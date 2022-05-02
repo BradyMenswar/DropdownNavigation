d015f47e8a6c1211dca4dac2fdcabe74b2fb3c7f
BradyMenswar <bradymenswar@gmail.com>
2022-05-02 11:07:00 -0400

    Finalized mobile design and ready to publish
    Finished the mobile layout and functionality.
    Added screenshot and edited readme file.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+    const isMobileButton = e.target.matches("[data-mobile-button]")
+    if(!isMobileButton && !isDropdownButton && (e.target.closest('[data-dropdown]') != null || e.target.closest('[data-mobile-menu]') != null)) return
 
     let currentDropdown
+    let currentMobileMenu
     if(isDropdownButton) {
+        console.log("TEST!")
         currentDropdown = e.target.closest('[data-dropdown]')
         currentDropdown.classList.toggle('active')  
+    }
+    if(isMobileButton) {
+        currentMobileMenu = e.target.closest('[data-mobile-menu]')
+        if(currentMobileMenu.classList.contains('active')) {
+            currentMobileMenu.classList.remove('active')
+        }
+        else {
+            currentMobileMenu.classList.toggle('active') 
+        }
+            
     }
 
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
         if(dropdown === currentDropdown) return
         dropdown.classList.remove('active')
     })
+
 })
 
